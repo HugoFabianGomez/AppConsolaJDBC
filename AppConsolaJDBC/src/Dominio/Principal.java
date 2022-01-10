3bc58516c9635499881fbca1496d7630e3970476
HugoFabianGomez <64662305+HugoFabianGomez@users.noreply.github.com>
2022-01-10 10:41:35 -0300

    Inicio Java

---
+++ AppConsolaJDBC/src/Dominio/Principal.java
@@ -0,0 +1,39 @@
+package Dominio;
+
+import java.util.ArrayList;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		UsuarioDao usuarioDao = new UsuarioDao();
+	
+		Usuario usuario1 = new Usuario();
+		usuario1.setNombre("Santy Hugo -");
+		usuario1.setApellido("Gomez");
+		usuarioDao.EjecutarSPA_CrearUsuario(usuario1);
+		
+		int filas = usuarioDao.agregarUsuario(usuario1);
+		
+		if(filas ==1)
+			System.out.println("Usuario Agregado");
+		else
+			System.out.println("Usuario no agregado");
+	
+	
+//		Usuario usuario2 = usuarioDao.ObtenerUsuario2(1);
+//		System.out.println(usuario2.toString());
+	
+	/*	
+		ArrayList<Usuario> ListaUsuarios = usuarioDao.ObtenerTodosUsuarios();
+		
+		for(Usuario user : ListaUsuarios)
+		{
+			System.out.println(user.toString());
+		}
+	*/	
+	} 
+	
+
+}
